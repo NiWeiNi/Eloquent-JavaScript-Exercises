097ac45363d66dbe5ab88867aa92ae5b3caff0d4
Wei <weinini007@gmail.com>
2018-03-13 00:44:18 +0100

    Add second requirement FizzBuzz

--- Chapter 2/ex2-2.js
+++ Chapter 2/ex2-2.js
@@ -1,4 +1,35 @@
+// Define variable
+let number = 1;
+
 // Define loop
-for (let i = 0; i <= 100; i++) {
-    
+for (let i = 0; i < 100; i++) {
+    if (number % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (number % 5 === 0 && number % 3 !== 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(number);
+    }
+    number++;
+}
+
+// Modify previous to print FizzBuzz for %3 and %5
+let newNumber = 1;
+
+for (let i = 0; i < 100; i++) {
+    if (newNumber % 3 === 0 && newNumber % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if(newNumber % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (newNumber % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(newNumber);
+    }
+    newNumber++;
 }
