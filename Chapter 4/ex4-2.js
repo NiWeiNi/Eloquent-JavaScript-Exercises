af8d278954fdc5ccadbd126972cc5c666fb29916
Wei <weinini007@gmail.com>
2018-03-17 00:31:15 +0100

    Change reverseArray to a function returning new var array

--- Chapter 4/ex4-2.js
+++ Chapter 4/ex4-2.js
@@ -1,17 +1,15 @@
 function reverseArray(array) {
-    let tempValue, j = array.length-1;
-    for (let i = 0; i <= array.length/2; i++) {
-        tempValue = array[i];
-        array[i] = array[j];
-        array[j] = tempValue;
+    let tempValue, j = array.length-1, newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray[j] = array[i] ;
         j -= 1; 
     }
-    return array;
+    return newArray;
 }   
 
 function reverseArrayInPlace(array) {
     let tempValue, j = array.length-1;
-    for (let i = 0; i <= array.length/2; i++) {
+    for (let i = 0; i < array.length/2; i++) {
         tempValue = array[i];
         array[i] = array[j];
         array[j] = tempValue;
