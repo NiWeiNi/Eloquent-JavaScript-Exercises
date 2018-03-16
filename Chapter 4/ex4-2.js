c9194a654da00a7d1790b8a04aa150d43706b87b
Wei <weinini007@gmail.com>
2018-03-17 00:22:11 +0100

    Fix reverseArray function

---
+++ Chapter 4/ex4-2.js
@@ -0,0 +1,14 @@
+function reverseArray(array) {
+    let tempValue, j = array.length-1;
+    for (let i = 0; i <= array.length/2; i++) {
+        tempValue = array[i];
+        array[i] = array[j];
+        array[j] = tempValue;
+        j -= 1; 
+    }
+    return array;
+}   
+
+function reverseArrayInPlace(array) {
+    for
+}
