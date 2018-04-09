7e9676f011301c905aa01340a620e3dacd3d3221
Wei <weinini007@gmail.com>
2018-04-10 00:46:24 +0200

    Add methods to Vec class

--- Chapter 6/ex6-1.js
+++ Chapter 6/ex6-1.js
@@ -1,3 +1,12 @@
-clas Vec {
-    
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(otherVec) {
+        return new Vec(otherVec.x + this.x, otherVec.y + this.y);
+    }
+    minus(otherVec) {
+        return new Vec(this.x - otherVec.x, this.y - otherVec.y);
+    }
 }
