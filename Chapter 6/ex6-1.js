bb2545d196977d96be83a88bb39802e48d190f75
Wei <weinini007@gmail.com>
2018-04-10 19:45:33 +0200

    Finish ex6-1

--- Chapter 6/ex6-1.js
+++ Chapter 6/ex6-1.js
@@ -1,12 +1,19 @@
+// Defines a vector
 class Vec {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
+    // Add two vectors
     plus(otherVec) {
         return new Vec(otherVec.x + this.x, otherVec.y + this.y);
     }
+    // Minus two vectors
     minus(otherVec) {
         return new Vec(this.x - otherVec.x, this.y - otherVec.y);
+    }
+    // Length of the vector to 0,0
+    get length() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
 }
