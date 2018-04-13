e764b6e04598f434212d7d712510782500237ab1
Wei <weinini007@gmail.com>
2018-04-13 19:39:12 +0200

    Add delete method

--- Chapter 6/ex6-2.js
+++ Chapter 6/ex6-2.js
@@ -1,4 +1,15 @@
 // Define a Group class, like Set
 class Group {
-    
+    constructor() {
+        this.arraySet = [];
+    }
+    // Create an add items method
+    add(element) {
+        if(!this.has(element))
+            this.arraySet.push(element);
+    }
+    // Reasign new group with all the elements except the eleemnt to delete 
+    delete(element) {
+        this.arraySet = this.arraySet.filter(el => el !== element);
+    }
 }
